d0caab3091ac9e2db5c48172f7cc6899cb10c7c8
bharath s <devbharu04@gmail.com>
2026-01-19 01:01:02 +0530

    new change

--- backend/models/Otp.model.js
+++ backend/models/Otp.model.js
@@ -6,7 +6,7 @@ const otpSchema = new mongoose.Schema(
             type: String,
             required: true
         },
-        otpHash: {
+        otp: {
             type: String,
             required: true
         },
--- backend/routes/textRoutes.js
+++ backend/routes/textRoutes.js
@@ -9,17 +9,13 @@ const router = Router();
 router.post("/addText", async (req, res) => {
     try {
         const { text } = req.body;
-
         const otp = randomNumber().toString();
-        const otpHash = await bcrypt.hash(otp, 10);
 
         await OtpModel.create({
             text,
             otp,
-            expiresAt: new Date(Date.now() + 5 * 60 * 1000)
+            expiresAt: new Date(Date.now() + 5 * 60 * 60 * 1000)
         });
-
-        console.log("OTP (testing):", otp);
 
         res.json({ message: "OTP sent", otp });
     } catch (err) {
@@ -32,25 +28,19 @@ router.post("/otp", async (req, res) => {
     try {
         const { otp } = req.body;
 
-        const record = await OtpModel.findOne({ otp })
+
+        const record = await OtpModel.findOne({
+            otp: otp,
+            expiresAt: { $gt: new Date() }
+        });
 
         if (!record) {
-            return res.status(400).json({ message: "OTP expired" });
+            return res.status(400).json({ message: "Invalid or expired OTP" });
         }
 
-        const isValid = await bcrypt.compare(otp, record.otpHash);
-
-        if (!isValid) {
-            return res.status(400).json({ message: "Invalid OTP" });
-        }
-
-        res.json({
-            success: true,
-            text: record.text
-        });
+        res.json({ success: true, text: record.text });
     } catch (err) {
         res.status(500).json({ message: "Server error" });
     }
 });
-
 export default router;
