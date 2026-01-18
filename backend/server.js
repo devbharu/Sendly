669e12ecfbc7e38989f1d7fe868a923459fce879
bharath s <devbharu04@gmail.com>
2026-01-19 00:35:14 +0530

    server.js change1

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const distPath = path.resolve(__dirname, "dist");
 app.use(express.static(distPath));
 
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(distPath, "index.html"), (err) => {
         if (err) {
 
